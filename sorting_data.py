4ea50c2b714cd3ae71b5d5ee5913ea16c5577e40
Joshua Lovin <jdlovin7185@gmail.com>
2023-01-11 21:42:09 -0500

    sorting data

---
+++ sorting_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('dataset.csv')
+# this will sort descending 0 to whatever loudness goes down to
+df_loudness = df.sort_values(by='loudness')
+
+# print(df_loudness)
+
+df_ascending_tempo = df.sort_values(['track_genre', 'tempo'], ascending=(1, 0))
+
+print(df_ascending_tempo)
