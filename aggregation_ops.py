c03a32295e403313356aaa2807a5d4097f274df0
Joshua Lovin <jdlovin7185@gmail.com>
2023-01-12 20:37:06 -0500

    aggregation and grouping ops

---
+++ aggregation_ops.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('dataset.csv')
+
+
+# aggregate function is used to to find the minimum and maximum values of the numerical values
+list1 = [col for col in df.columns if df[col].dtype in ['float', 'int64']]
+aggregate = df[list1].agg(['min', 'max'])
+print(aggregate)
---
+++ grouping_ops.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv('dataset.csv')
+
+group_by_track_genre = df.groupby(['track_genre']).count()[['artists']]
+
+# print(group_by_track_genre)
+
+grouped_multiple = df.groupby(['artists', 'track_genre']).agg({'popularity': ['mean', 'min', 'max']})
+
+grouped_multiple.columns = ['popular_mean', 'popular_min', 'popular_max']
+
+grouped_multiple = grouped_multiple.reset_index()
+
+print(grouped_multiple.head())
