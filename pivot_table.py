20b4766f4812355ed2dc7e88ded6cfc88a213de9
Josh Lovin <jdlovin7185@gmail.com>
2023-01-14 16:06:52 -0500

    pivot table

---
+++ pivot_table.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('dataset.csv')
+
+# you can create a table using the pivot table func along with numpy to get a bunch
+# of data that you want 'math-ed over'
+pivot1 = pd.pivot_table(df, index='artists', aggfunc=np.mean)
+print(pivot1)
