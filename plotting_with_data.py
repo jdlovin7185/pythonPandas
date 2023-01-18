e2220ba7674cbba6023d0cbe9b7502bb0f8aa84f
Joshua Lovin <jdlovin7185@gmail.com>
2023-01-18 04:46:33 -0500

    added a data set

--- plotting_with_data.py
+++ plotting_with_data.py
@@ -1,4 +1,24 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-pd.read_csv('dataset.csv')
+df = pd.read_csv('2019_happy_data.csv')
 
+# giving the info for the dataframe
+df.info()
+
+# replacing null values in 'popularity' with its mean value
+# df['popularity'].fillna(df['popularity'].mean(), inplace=True)
+
+# making a boxplot
+fig = plt.figure(figsize=[6, 6])
+
+# setting the axis
+ax = fig.add_axes([0, 0, 1, 1])
+ax.set_title('Distribution of Social Support vs Generosity')
+
+# plotting the boxplot
+ax.boxplot(df['Social support'])
+
+# fig.show()
+#
+plt.show()
